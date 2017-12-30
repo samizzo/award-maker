bfa4ca38c9fce5b3592ddd95c9b3967957e5f6b8
Sam Izzo <sam@horsedrawngames.com>
2017-12-30 16:48:40 +1100

    Fixed missing comma

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ requirejs.config({
     'baseUrl': 'js',
     'paths': {
         'jquery': 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min',
-        'mustache': 'https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.min'
+        'mustache': 'https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.min',
         'clipboard': 'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.7.1/clipboard.min'
     }
 });
