96f2e432dc97ad318ba1289a472c8f4c76a727c0
Sam Izzo <sam@humbug.net>
2020-05-30 15:30:47 +1000

    Fixed busted filesaver.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ requirejs.config({
         'jquery': 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min',
         'mustache': 'https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.min',
         'clipboard': 'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.7.1/clipboard.min',
-        'filesaver': 'https://fastcdn.org/FileSaver.js/1.1.20151003/FileSaver.min',
+        'filesaver': 'https://cdn.rawgit.com/eligrey/FileSaver.js/5ed507ef8aa53d8ecfea96d96bc7214cd2476fd2/FileSaver.min',
         'canvastoblob': 'https://cdnjs.cloudflare.com/ajax/libs/javascript-canvas-to-blob/3.14.0/js/canvas-to-blob.min',
         'uikit': 'https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.4/js/uikit.min'
     },
